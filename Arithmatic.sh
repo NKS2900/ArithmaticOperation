073e0caa062d0e6252f1e3be1d9e0a16ee5c5c15
unknown <nksayyad2900.com>
2020-10-12 00:06:02 +0530

    Added: performing operaitons

--- Arithmatic.sh
+++ Arithmatic.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -x
-echo "Arithmetic Compution And Sorting"
+echo "Arithmetic Compution And Sorting "
 
 #TO TAKING INPUT FROM USER
 read -p "Enter first input :" firstInput
 read -p "Enter second input:" secondInput
 read -p "Enter third input :" thirdInput
 
+#COMPUTE ARITHMETIC OPERATION
+result=`echo "scale=2;$firstInput + $secondInput * $thirdInput" | bc`
+echo "Result:" $result
