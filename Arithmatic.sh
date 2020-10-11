2575280244e249d1ab3ef1653dab6a901aac4199
unknown <nksayyad2900.com>
2020-10-11 23:57:23 +0530

    Added: Taking input

--- Arithmatic.sh
+++ Arithmatic.sh
@@ -1,1 +1,8 @@
-echo arithmatic operations
+#!/bin/bash -x
+echo "Arithmetic Compution And Sorting"
+
+#TO TAKING INPUT FROM USER
+read -p "Enter first input :" firstInput
+read -p "Enter second input:" secondInput
+read -p "Enter third input :" thirdInput
+
